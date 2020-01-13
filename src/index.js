29e45e3a5cb9c83bf687cdc9b10e5dd3f9ff1d09
veysabe <veysabe@gmail.com>
2020-01-13 13:56:47 +0300

    Backup for bootstrap-select

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles/App.css";
 import Header from "./components/Header";
-
 
 ReactDOM.render(<Header />, document.body);
